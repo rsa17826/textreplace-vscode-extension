7d7da196dae99e996551e8b39f32de37f8f9bb51
Your Name <you@example.com>
2026-04-01 12:39:46 -0400

    added windows quick install script

---
+++ windows auto build script.ps1
@@ -0,0 +1,21 @@
+cd $HOME\Desktop
+if (Test-Path build_temp) { rm -Recurse -Force build_temp }
+mkdir build_temp; cd build_temp
+git clone https://github.com/rsa17826/textreplace-vscode-extension .
+
+$nodeDir = "$HOME\Desktop\node-v22.0.0-win-x64"
+if (-not (Test-Path $nodeDir)) {
+  Write-Host "Downloading Portable Node v22..." -ForegroundColor Cyan
+  Invoke-WebRequest -Uri "https://nodejs.org/dist/v22.0.0/node-v22.0.0-win-x64.zip" -OutFile "node.zip"
+  Expand-Archive node.zip -DestinationPath $HOME\Desktop
+  rm node.zip
+}
+
+node -v
+
+npm install
+echo y|npx @vscode/vsce package --allow-missing-repository --no-git-tag-version
+
+mv *.vsix ..
+cd ..
+rm -Recurse -Force build_temp
